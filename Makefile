bee32127de5db3614a8e41989f5dda2277647b5b
Zeineb Zayet <zeineb.zayet1@gmail.com>
2021-11-21 08:00:24 -0800

    sjf

--- Makefile
+++ Makefile
@@ -3,8 +3,9 @@ BUILD_DIR := algorithms/build
 SRC_FILES := $(wildcard $(SRC_DIR)/*.c)
 
 all: $(SRC_FILES:$(SRC_DIR)/%.c=$(BUILD_DIR)/%) main
-main:
+main: main.c
 	gcc main.c -o main
 
 $(BUILD_DIR)/%: $(SRC_DIR)/%.c
+	
 	gcc $< -o $@
--- algorithms/src/priority.c
+++ algorithms/src/priority.c
@@ -1,9 +1,8 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void sjf(char config[]){
+void priority(char config[]){
    FILE *fp;
    fp = fopen(("./%s", config), "r");
    if(fp == NULL) {
@@ -33,7 +32,7 @@ void sjf(char config[]){
    
    for(int i=0; i < 3; i++){ 
       for(int j=2; j > i; j--){
-         if(atoi(tab[j][1]) < atoi(tab[j-1][1]) || ((atoi(tab[j][1]) == atoi(tab[j-1][1])) && (atoi(tab[j][2]) < atoi(tab[j-1][2])))){
+         if(atoi(tab[j][1]) < atoi(tab[j-1][1]) || ((atoi(tab[j][1]) == atoi(tab[j-1][1])) && (atoi(tab[j][3]) > atoi(tab[j-1][3])))){
             for (int r = 0; r < 4; r++){
                char *t = tab[j - 1][r];
                tab[j - 1][r] = tab[j][r];
@@ -45,7 +44,6 @@ void sjf(char config[]){
 
 int t_a = atoi(tab[0][1]), t_e;
    for(int k=0; k < 3; k++){ 
-          printf("%s \n",tab[k][1]);
       int start = t_a, finish = t_a;
       t_a = atoi(tab[k][1]);
       t_e = atoi(tab[k][2]);
@@ -53,7 +51,7 @@ int t_a = atoi(tab[0][1]), t_e;
       finish += t_e + idle;
        
       for(int q=k+1; q < 2; q++){
-          if((atoi(tab[q][1]) <= finish && atoi(tab[q+1][1])<=finish) && (atoi(tab[q][2]) > atoi(tab[q+1][2]))){
+          if((atoi(tab[q][1]) <= finish && atoi(tab[q+1][1])<=finish) && (atoi(tab[q][3]) < atoi(tab[q+1][3]))){
             for (int r1 = 0; r1 < 4; r1++){
                char *t1 = tab[q + 1][r1];
                tab[q + 1][r1] = tab[q][r1];
@@ -63,13 +61,7 @@ int t_a = atoi(tab[0][1]), t_e;
       }
    }
   
-       
-     
-   
-
-       
-   
-   
+    
    int ta = atoi(tab[0][1]), te;
    int start = ta, finish = ta;
    for(int i=0; i < 3; i++){
@@ -88,7 +80,7 @@ int main(int argc, char *argv[]) {
    if(argc == 1)
       printf("Usage: %s <config>\n", argv[0]);
    else{
-      sjf(argv[1]);
+      priority(argv[1]);
    }
    return 0;
 }
