fcf4748c189fca70fbac669fe9841255667a1cf6
Zeineb Zayet <zeineb.zayet1@gmail.com>
2021-12-09 07:34:27 -0800

    test

--- algorithms/src/RR.c
+++ algorithms/src/RR.c
@@ -3,14 +3,56 @@
 
 void RR(char configFile[],int q){
    struct node *processesList = getProcessesListFromFile(configFile);
+    printProcessTable(processesList);
    bubbleSort(processesList, 1, false); // Asc Sort List Based On TA (which is on index 1 in DATA array)
-   struct Queue *queue = createQueueFromLinkedList(processesList);
+   struct Queue *queue = createQueueFromLinkedList(processesList);  
+   struct node *tmp=sortByTaPreemptive(queue,q);
+   addIdleNodes(tmp);
+   printGanttChart(tmp, "Round Robin");
+
+  
+
+   /*while(queue->front){
+
+       printf("%s \n",queue->front->data[0]);
+       printf("%s \n",queue->front->data[1]);
+       printf("%s \n",queue->front->data[2]);
+       printf("%s \n",queue->front->data[3]);
+              printf("******************** \n");
+              queue->front=queue->front->next;
+   }*/
+   /*while(queue->front){
+
+   struct node *process = (struct node*)malloc(sizeof(struct node)); 
+
+   for(int i=0;i<=3;i++){
+      process->data[i]=(char*)malloc(5*sizeof(char));
+   }
+
+   strcpy(process->data[0], queue->front->data[0]);
+   strcpy(process->data[1], queue->front->data[1]);
+   strcpy(process->data[2], queue->front->data[2]);
+   strcpy(process->data[3], queue->front->data[3]);
    
-   //struct node *tmp = processesList;
-   //sortByTaPreemptive(tmp, 1, 2,q);
-   //tmp = processesList;
-   //displayResult(tmp);
-}  
+   
+   
+   struct node *Node = (struct node*)malloc(sizeof(struct node));
+   
+   Node=newNode(process);
+         if (Res== NULL){
+            Res=Node;
+         }
+         else{
+            while (tmp->next != NULL){
+               tmp = tmp->next;
+            }
+         tmp->next = Node;
+         }
+   queue->front=queue->front->next;
+   }
+   affiche1(Res);*/
+
+ }
 
 int main(int argc, char *argv[]) {
     int Q;
--- algorithms/src/main.h
+++ algorithms/src/main.h
@@ -8,17 +8,16 @@ struct node {
    struct node *next;
 };
  
-// The queue, front stores the front node of LL and rear stores the
-// last node of LL
+// The queue, front stores the front node of LL and rear stores the last node of LL
 struct Queue {
    struct node *front, *rear;
 };
  
 
-/* Function to bubble sort the given linked list based on an index to compare with*/
+/* Function to bubble sort the given linked list based on an index to compare with */
 void bubbleSort(struct node *start, int comparisonIndex, bool isDesc);
   
-/* Function to swap data of two nodes a and b*/
+/* Function to swap data of two nodes a and b */
 void swap(struct node *a, struct node *b);
 
 /* Function to reverse a linked list recursively */
@@ -27,13 +26,13 @@ struct node* inverseLinkedList(struct node *node);
 /* Function that reads processes from config file and returns linked list of processes */
 struct node *getProcessesListFromFile(char *configFile);
 
-/* function to remove white spaces from string */
+/* Function to remove white spaces from string */
 char *remove_white_spaces(char *str);
 
-/* Function to sort by TA and TE */
+/* Function to sort by TA and TE or Priority */
 void bubbleSortByTwoIndexes(struct node *start, int comparisonIndex1, int comparisonIndex2, bool Desc);
 
-/* Function to sort by TE */
+/* Function to sort by TE or Priority */
 void sortByTwoIndexes(struct node *head, int comparisonIndex1, int comparisonIndex2, bool Desc);
 
 /* Function to print Gantt chart */
@@ -44,6 +43,31 @@ void printProcessTable(struct node *head);
 
 /* Function to add Idle nodes to print them in gantt diagram */
 void addIdleNodes(struct node *head);
+
+/* Function to sort process by TA preemptive */
+struct node *sortByTaPreemptive(struct Queue *queue, int quantum);
+
+/* Function to create an empty Queue */
+struct Queue* createQueue();
+
+/* The function to add an element to queue q */
+void enQueue(struct Queue* q, struct node *element);
+
+/* Function to remove element from given queue q */
+struct node* deQueue(struct Queue* q);
+
+/* Function to create Queue from LinkedList */
+struct Queue *createQueueFromLinkedList(struct node *head);
+
+/* Function to create a new linked list node */
+struct node* newNode(struct node *dataNode);
+
+/* The function to add an element to queue q */
+void enQueue1(struct Queue* q, struct node *element);
+
+void affiche1(struct node*res);
+
+
 
 void printProcessTable(struct node *head){ 
    printf("\n******************** Processes Table *********************\n\n");
@@ -113,12 +137,14 @@ void printGanttChart(struct node *head, char *algorithmName){
 void addIdleNodes(struct node *head){
    struct node *tmp = head->next, *prev = head;
    int finish = atoi(head->data[1]) + atoi(head->data[2]);
-   while(tmp && tmp->next){
+   while(tmp){
       int ta = atoi(tmp->data[1]);
       int te = atoi(tmp->data[2]);
       int idle = (ta > finish) ? (ta - finish) : 0;
       if(idle > 0){
          struct node *newNode = (struct node*)malloc(sizeof(struct node));
+         for(int i=0; i<4; i++)
+            newNode->data[i] = (char *)malloc(sizeof(char) * 100);
          newNode->data[0] = "-";
          sprintf(newNode->data[1], "%d", finish);
          sprintf(newNode->data[2], "%d", idle);
@@ -282,19 +308,34 @@ struct Queue *createQueueFromLinkedList(struct node *head){
    struct node *tmp = head;
    struct Queue *queue = createQueue();
    while(tmp){
-      enQueue(queue, tmp);
+      enQueue1(queue, tmp);
       tmp = tmp->next;
    }
    return queue;
 }
 
+// The function to add a key k to q
+void enQueue1(struct Queue *q, struct node *element){
+   // Create a new LL node
+   struct node *temp = newNode(element);
+   // If queue is empty, then new node is front and rear both
+   if (q->rear == NULL) {
+      q->front = q->rear = temp;
+      return;
+   }
+   q->rear->next = temp;
+   q->rear=temp;
+}
+
+
+
 // A utility function to create a new linked list node.
 struct node* newNode(struct node *dataNode){
     struct node* temp = (struct node*)malloc(sizeof(struct node));
-    temp->data[0] = dataNode->data[0];
-    temp->data[1] = dataNode->data[1];
-    temp->data[2] = dataNode->data[2];
-    temp->data[3] = dataNode->data[3];
+    temp->data[0] = strdup(dataNode->data[0]);
+    temp->data[1] = strdup(dataNode->data[1]);
+    temp->data[2] = strdup(dataNode->data[2]);
+    temp->data[3] = strdup(dataNode->data[3]);
     temp->next = NULL;
     return temp;
 } 
@@ -308,92 +349,151 @@ struct Queue* createQueue(){
 
 
 // The function to add a key k to q
-void enQueue(struct Queue* q, int k){
-    // Create a new LL node
-    struct node* temp = newNode(k);
-    // If queue is empty, then new node is front and rear both
-    if (q->rear == NULL) {
-        q->front = q->rear = temp;
-        return;
-    }
- 
-    // Add the new node at the end of queue and change rear
-    q->rear->next = temp;
-    q->rear = temp;
+void enQueue(struct Queue *q, struct node *element){
+   // Create a new LL node
+   struct node *temp = newNode(element);
+   // If queue is empty, then new node is front and rear both
+   if (q->rear == NULL) {
+      q->front = q->rear = temp;
+      return;
+   }
+   if(atoi(q->rear->data[1]) <= atoi(temp->data[1])){
+      q->rear->next = temp;
+      q->rear = temp;
+      return;
+   }
+   struct node *tmp = q->front->next, *prev = q->front;
+   while(tmp  && tmp->next && (atoi(tmp->data[1]) <= atoi(temp->data[1])) ){
+      tmp = tmp->next;
+      prev = prev->next;
+   }
+   temp->next= prev->next;
+   prev->next = temp;
 }
  
-// Function to remove a key from given queue q
-void deQueue(struct Queue* q){
+// Function to remove element from given queue q
+struct node* deQueue(struct Queue* q){
    // If queue is empty, return NULL.
    if (q->front == NULL)
-      return;
+     return q->front;
    // Store previous front and move front one node ahead
    struct node* temp = q->front;
-   struct node *ret = q->front;
+   //struct node *ret = q->front;
    q->front = q->front->next;
 
    // If front becomes NULL, then change rear also as NULL
    if (q->front == NULL)
       q->rear = NULL;
-   return temp;
+   free(temp);
+   return (q->front);
 }
 
+void affiche(struct Queue*q){
+    printf("********* Affichage de queue apres suppression du 1er element*********** \n");
+
+      while(q->front){
+
+       printf("%s \n",q->front->data[0]);
+       printf("%s \n",q->front->data[1]);
+       printf("%s \n",q->front->data[2]);
+       //printf("%s \n",queue->front->data[3]);
+      printf("******************** \n");
+       q->front=q->front->next;
+   } 
+}
+
+
+void affiche1(struct node*res){
+    printf("********* Affichage de Resultat*********** \n");
+
+      while(res){
+
+       printf("%s \n",res->data[0]);
+       printf("%s \n",res->data[1]);
+       printf("%s \n",res->data[2]);
+       printf("%s \n",res->data[3]);
+      printf("******************** \n");
+       res=res->next;
+   } 
+}
 
 struct node *sortByTaPreemptive(struct Queue *queue, int quantum){
+   
    struct node *Res = NULL;
-   while(queue){
-      struct node *process = (struct node*)malloc(sizeof(struct node));
-      strcpy(process->data[0], queue->front->data[0]);
-      strcpy(process->data[1], queue->front->data[1]);
-      strcpy(process->data[2], queue->front->data[2]);
-      strcpy(process->data[3], queue->front->data[3]);
-      deQueue(queue);
-      struct node *newNode = (struct node*)malloc(sizeof(struct node));
-      if(atoi(process->data[2]) > quantum){
-         sprintf(newNode->data[1], "%d", finish);
-         sprintf(newNode->data[2], "%d", quantum);
-         process->data[2] -= quantum;
-         process->data[1] = finish;
+   int finish=atoi(queue->front->data[1]),exec;
+   struct node *tmp;
+   struct node *frnt;
+   struct node *process = (struct node*)malloc(sizeof(struct node));
+   struct node *Node;
+
+
+   while(queue->front){
+
+      process->data[0] = strdup(queue->front->data[0]);
+      process->data[1] = strdup(queue->front->data[1]);
+      process->data[2] = strdup(queue->front->data[2]);
+      process->data[3] = strdup(queue->front->data[3]);
+
+      frnt=deQueue(queue);
+     
+      Node = (struct node*)malloc(sizeof(struct node));
+      
+      if(atoi(process->data[2]) <= quantum){
+         //finish += atoi(process->data[2]);
+         //sprintf(process->data[1], "%d", finish);
+   
+         Node=newNode(process);
+         if (Res == NULL){
+            Res=Node;
+            tmp=Res;
+         }
+         else{
+         tmp->next = Node;
+         tmp=tmp->next;
+         }
+
+         printf("***** queue *****");
+         affiche(queue);
+         affiche1(Res);
+    
       }else{
 
+            //sprintf(process->data[2], "%d", exec);    
+            Node=newNode(process);
+            sprintf(Node->data[2],"%d",quantum);
+            if (Res == NULL){
+               Res=Node;
+               tmp=Res;
+            }
+            else{
+               tmp->next = Node;
+               tmp=tmp->next;
+
+            }
+
+           
+            exec= atoi(process->data[2]) - quantum;
+            sprintf(process->data[2], "%d", exec); 
+            finish += atoi(process->data[1])+quantum;
+            sprintf(process->data[1], "%d", finish);
+
+             
+   
+            /*printf("%s \n",process->data[0]);
+            printf("%s \n",process->data[1]);
+            printf("%s \n",process->data[2]);
+            printf("%s \n",process->data[3]);*/
+       
+            enQueue(queue,process);
+            printf("***** queue *****");
+            affiche(queue);
+            affiche1(Res);
       }
-      while(finish <= queue->front->data[1] && ){
-
-      }
-
-   }
-   // linked list to save the first process w nhot el ta w Q feha el if
-   // insertFin(Res,head->data); 
-
-   // while(head){ 
-   //    int finish;
-   //    if(atoi(head->data[2]) < quantum)
-   //       finish = atoi(head->data[comparisonIndex1]) + atoi(head->data[comparisonIndex2]);
-   //    else
       
-   //       finish = atoi(head->data[comparisonIndex1]) + quantum;
-      
-   //    //insert head in file
-   //    enfiler(f,head->data);
-   //    struct node *head2 = head->next;
-   //    //delete head in file
-   //    defiler(f,head->data);
-   //    while(head2){
-   //       if(atoi(head2->data[comparisonIndex1]) <= finish )  
-   //       {          
-   //       //insert head2 in file 
-   //       enfiler(f,head2->data);
-   //       }        
-   //       head2 = head2->next;
-   //    }
-   //    // insert head in file
-   //    enfiler(f,head->data);
-   //    //ken te wfe ena7ih ml file
-      
-   //    //aka linked list nsavi feha e theny eli howa awel element f file ndefilih w aprés naawed n'emplih ken mazel makamalesh e te
-   //    insertFin(Res,f->tete->data);
+      queue->front=frnt;
 
-   //    head = head->next;
-   // }
-   return Res;
 }
+return Res;
+}
+
+
